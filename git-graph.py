b1e289f691a456659eac427d58cf8e6417447d31
Michael Goerz <goerz@physik.fu-berlin.de>
2009-08-17 01:59:24 +0200

    Added OptionParser

--- git-graph.py
+++ git-graph.py
@@ -2,6 +2,7 @@
 import os
 import re
 import sys
+from optparse import OptionParser
 
 
 BOLD = '\033[1m'
@@ -22,12 +23,13 @@ def get_refs():
         else:
             print "Unexpected ref format"
             print line
-            sys.exit()
+            return None
     return ref_for_hash
 
 
 def main(options=None):
     ref_for_hash = get_refs()
+    if ref_for_hash is None: return 1
     graph = os.popen(
         'git log --all --pretty=format:\'%H %h : %an %ai %n %s%n\' '+
         '--date-order --graph', 'r', 0)
@@ -42,6 +44,7 @@ def main(options=None):
         graphmatch = pgraph1.match(line)
         if graphmatch:
             if ref_for_hash.has_key(graphmatch.group('longhash')):
+                hash = graphmatch.group('hash')
                 ref = ref_for_hash[graphmatch.group('longhash')]
                 if ref.startswith('remotes/'):
                     ref = ref.replace('remotes/', 'r:', 1)
@@ -53,7 +56,7 @@ def main(options=None):
                     ref = BOLD + RED + '[' + ref + ']' + ENDC
                 print "%s%s : %s %s" % (
                     graphmatch.group('graph'),
-                    graphmatch.group('hash'),
+                    hash,
                     ref,
                     graphmatch.group('info'),
                 )
@@ -75,8 +78,19 @@ def main(options=None):
             else:
                 print "Unexpected graph format"
                 print line
-                sys.exit()
+                return 1
+    return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    arg_parser = OptionParser(usage = "git_graph.py [options] [path] ",
+                              description = __doc__)
+        
+    arg_parser.add_option('--no-hash', action='store_true', dest='no_hash',
+                        default=False, help="Don't print the commit hash")
+    options, args = arg_parser.parse_args(sys.argv)
+
+    if len(args) > 1:
+        os.chdir(args[1])
+
+    sys.exit(main(options))
